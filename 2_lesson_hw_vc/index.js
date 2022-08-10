73c49c9ba3e730326dd23243573e9bc40f02d0b0
User <lejlagorcevic02@gmail.com>
2022-08-10 22:35:54 +0200

    js

--- 2_lesson_hw_vc/index.js
+++ 2_lesson_hw_vc/index.js
@@ -1,12 +1,86 @@
 //Write a program that prompts the user to input a character 
 //and determine the character is vowel or consonant.
-letter = prompt('Insert a letter:');
+input = prompt('Insert a letter:');
 
-if (letter = "a","e","i","o","u" ) {
-    console.log('The character you inserted is a vowel.')
+
+if (input === "e"){
+    console.log("The character you inserted is a vowel.");
+}
+if (input === "i") {
+    console.log("The character you inserted is a vowel.")
+}
+if (input === "o"){
+    console.log("The character you inserted is a vowel.");
+}
+if (input === "u") {
+    console.log ("The character you inserted is a vowel.")
+}
+else if(input === "q"){
+    console.log ("The character is a constonant.")
+}
+else if(input === "w"){
+    console.log ("The character is a constonant.")
+}
+else if(input === "r"){
+    console.log ("The character is a constonant.")
+}
+else if(input === "t"){
+    console.log ("The character is a constonant.")
+}
+else if(input === "y"){
+    console.log ("The character is a constonant.")
+}
+else if(input === "u"){
+    console.log ("The character is a constonant.")
+}
+else if(input === "p"){
+    console.log ("The character is a constonant.")
+}
+else if(input === "s"){
+    console.log ("The character is a constonant.")
+}
+else if(input === "s"){
+    console.log ("The character is a constonant.")
+}
+else if(input === "d"){
+    console.log ("The character is a constonant.")
+}
+else if(input === "f"){
+    console.log ("The character is a constonant.")
+}
+else if(input === "g"){
+    console.log ("The character is a constonant.")
+}
+else if(input === "h"){
+    console.log ("The character is a constonant.")
+}
+else if(input === "j"){
+    console.log ("The character is a constonant.")
+}
+else if(input === "l"){
+    console.log ("The character is a constonant.")
+}
+else if(input === "z"){
+    console.log ("The character is a constonant.")
+}
+else if(input === "x"){
+    console.log ("The character is a constonant.")
+}
+else if(input === "c"){
+    console.log ("The character is a constonant.")
+}
+else if(input === "v"){
+    console.log ("The character is a constonant.")
+}
+else if(input === "b"){
+    console.log ("The character is a constonant.")
+}
+else if(input === "n"){
+    console.log ("The character is a constonant.")
+}
+else if(input === "m"){
+    console.log ("The character is a constonant.")
 }
 
-if (letter = "q","w","r","t","y","p","s","d","f","g","h","j",
-             "k","l","z","x","c","v","b","n","m"){
-  console.log('The character you inserted is a consonant.')
-}
+else if(input === !"a" || input === !"e" || input === !"i" || input === !"o" || input === !"u"){
+    console.log("consonant")}
---
+++ 2_lesson_hw_vc2/index.js
@@ -0,0 +1,10 @@
+//Write a program that prompts the user to input a character 
+//and determine the character is vowel or consonant.
+unos = prompt("unesi")
+
+if(unos =='a' || unos =='e' || unos =='i' || unos =='o' || unos =='u')
+  {console.log("Vowel")}
+
+else{
+    console.log("Consonant")
+}
